17e93d2ba9933e7abaeaea445c904b0a310d017d
Jidnyasa Bhosale <138592872+Jidnyasa21@users.noreply.github.com>
2024-11-27 09:08:20 +0530

    Create assig1_4.cpp

---
+++ assig1_4.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Customer {
+private:
+    string name;
+    int calls;
+    double bill;
+
+public:
+    void inputData() {
+        cout << "Enter customer name: ";
+        getline(cin, name);
+        cout << "Enter number of calls: ";
+        cin >> calls;
+    }
+
+    void computeBill() {
+        if (calls <= 100) {
+            bill = 200;
+        } else if (calls <= 150) {
+            bill = 200 + (calls - 100) * 0.60;
+        } else if (calls <= 200) {
+            bill = 200 + 50 * 0.60 + (calls - 150) * 0.50;
+        } else {
+            bill = 200 + 50 * 0.60 + 50 * 0.50 + (calls - 200) * 0.40;
+        }
+    }
+
+    void displayData() {
+        cout << "Customer Name: " << name << endl;
+        cout << "Number of Calls: " << calls << endl;
+        cout << "Bill Amount: Rs. " << bill << endl;
+    }
+};
+
+int main() {
+    Customer c;
+    c.inputData();
+    c.computeBill();
+    c.displayData();
+    return 0;
+}
